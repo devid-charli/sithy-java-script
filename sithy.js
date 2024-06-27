31dfd1324f0fc485d1efd5696b1c7f0cd5a687de
devid-charli <mustakali444888@gmail.com>
2024-06-27 03:06:02 -0700

    type of js

--- sithy.js
+++ sithy.js
@@ -15,3 +15,19 @@ console.log(myAge)
 
 var mangoName ="rupali"
 console.log( mangoName)
+
+
+var myVIllage ='korapara'
+console.log(myVIllage)
+
+var myDog =`rasti`
+console.log(myDog)
+
+var myVIllage ="ABC"
+console.log(myVIllage)
+
+
+
+var myCcow ='bala'
+console.log(typeof  myCcow);
+
